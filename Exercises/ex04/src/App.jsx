c0d0e46aa4af5ac7aed6aa89c8b5b82adb1122d7
PabloSantos-CG <comercial-pablo@outlook.com>
2023-10-17 09:00:40 -0300

    Daily Exercise

--- Exercises/ex04/src/App.jsx
+++ Exercises/ex04/src/App.jsx
@@ -1,27 +1,56 @@
 import { useState } from "react"
 
+
 export default function App() {
-  const [generate, setGenerate] = useState('')
+  const [password, setPassword] = useState()
+  const [textBtnCopy, setTextBtnCopy] = useState('Copiar')
   
+  function passwordGenerate() {
+    const uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz"
+    const numbersCharacters = "0123456789"
+    const specialCharacters = "!@#$%^&*()_+-=[]{}|:;<>,.?/~"
+
+    let newPassword = ''
+
+    for (let i = 0; i < 3; i++) {
+      const positionUpperCase = Math.floor(Math.random()* uppercaseCharacters.length)
+      const positionLowerCase = Math.floor(Math.random()* lowercaseCharacters.length)
+      const positionNumberChar = Math.floor(Math.random()* numbersCharacters.length)
+      const positionSpecialChar = Math.floor(Math.random()* specialCharacters.length)
+
+      newPassword += uppercaseCharacters[positionUpperCase] + lowercaseCharacters[positionLowerCase] + numbersCharacters[positionNumberChar] + specialCharacters[positionSpecialChar]
+    }
+    
+    setPassword(newPassword)
+    setTextBtnCopy('Copiar')
+  }
+
+  function copyPassword() {
+    window.navigator.clipboard.writeText(password)
+    setTextBtnCopy('Copiado!')
+  }
+
   return (
     <div className="container">
       <h1>Gerador de Senhas</h1>
 
       <div className="buttonContainer">
         <button
-          
+          onClick={passwordGenerate}
         >
           Gerar!
         </button>
 
         <button
+          onClick={copyPassword}
         >
-          Copiar
+          {textBtnCopy}
         </button>
 
       </div>
 
-      <input type="text" disabled id="password"/>
+      <input type="text" disabled value={password}/>
     </div>
   )
 }
