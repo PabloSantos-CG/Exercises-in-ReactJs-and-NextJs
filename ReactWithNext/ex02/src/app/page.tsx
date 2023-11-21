13d4ffcffb6b6e823772e5eba11a51a393e54d5b
PabloSantos-CG <comercial-pablo@outlook.com>
2023-11-21 11:43:13 -0300

    Exercise

--- ReactWithNext/ex02/src/app/page.tsx
+++ ReactWithNext/ex02/src/app/page.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+"use client";
+import { User } from "@/types/User";
+import { useEffect, useState } from "react";
 
-type Props = {}
+export default function () {
+  const [users, setUsers] = useState<User[]>([]);
 
-export default function ({}: Props) {
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((value) => {
+        setUsers(value);
+        console.log(users);
+      });
+  }, []);
+
   return (
-    <div>
-      
+    <div className="
+      h-screen
+      flex flex-col justify-center items-center
+      bg-slate-300
+    ">
+      <h1 className="text-xl mb-2">Usuários no sistema</h1>
+      <ul className="
+        max-h-96
+        rounded
+        p-2
+        overflow-auto
+        bg-white
+        shadow-sm
+      ">
+        {users.map(user => 
+          <li key={user.id}>
+            Nome e Sobrenome: {user.name} <br/>
+            Username: {user.username} <br/>
+            E-mail: {user.email} <br/>
+            <br />
+          </li>
+        )}
+      </ul>
     </div>
-  )
+  );
 }
