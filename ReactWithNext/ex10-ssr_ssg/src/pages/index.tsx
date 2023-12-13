18cb26b0ef6698053cfc02ef5bf53d11a66d4714
PabloSantos-CG <comercial-pablo@outlook.com>
2023-12-13 16:21:10 -0300

    Update

--- ReactWithNext/ex10-ssr_ssg/src/pages/index.tsx
+++ ReactWithNext/ex10-ssr_ssg/src/pages/index.tsx
@@ -1,14 +1,28 @@
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { Data } from "./api/hello";
+import { GetServerSideProps } from "next";
 
-export default function Home() {
+export const getServerSideProps: GetServerSideProps = async () => {
+  const serverSideRedering: Data = await fetch(
+    `${process.env.NEXT_PUBLIC_APIURL}/api/hello`
+  ).then((res) => res.json());
+
+  return {
+    props: { serverSideRedering },
+  };
+};
+
+export default function Home(props: {
+  children?: ReactNode;
+  serverSideRedering?: Data;
+}) {
   const [clientSide, setClientSide] = useState<Data>();
 
   useEffect(() => {
-    fetchData();
+    clientSideRendering();
   }, []);
 
-  const fetchData = async () => {
+  const clientSideRendering = async () => {
     const data = await fetch("/api/hello").then((res) => res.json());
     setClientSide(data);
   };
@@ -18,12 +32,12 @@ export default function Home() {
       <h1 className="text-center">Testando renderizações</h1>
       <div className="flex justify-center m-6 gap-x-40">
         <div>
-          <p className="font-bold">CLIENT:</p>
-          <p>Hora da requisição: {clientSide?.date.toString()}</p>
+          <p className="font-bold">Gerado no CLIENT:</p>
+          <p>Hora da requisição: {clientSide?.date}</p>
         </div>
         <div>
-          <p className="font-bold">SERVER:</p>
-          <p>Linha 2</p>
+          <p className="font-bold">Gerado no SERVER:</p>
+          <p>Hora da requisição: {props.serverSideRedering?.date}</p>
         </div>
       </div>
     </div>
